23f785739006695ab913b8f42f269142277b7acf
TheDummyUser <abhiram.reddy122002@gmail.com>
2024-04-25 23:17:46 +0530

    margin...

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ const page = () => {
   return (
     <div>
       <header className="flex justify-between p-5 max-w-7xl mx-auto border-b-2">
-        <div>
-          <p>Hello User's</p>
+        <div className='lg:text-xl text-xs'>
+          <Link href="/" className='underline underline-offset-4 hover:text-blue-500 hover:text-xl'>Hello User's</Link>
         </div>
-        <div className="flex gap-4">
-          <Link href="#">/About me</Link>
-          <Link href="#">/Blogs</Link>
+        <div className="flex gap-4 lg:text-xl text-xs">
+          <Link href="#" className='underline underline-offset-4 hover:text-blue-500 hover:text-xl'>/About me</Link>
+          <Link href="#" className='underline underline-offset-4 hover:text-blue-500 hover:text-xl'>/Blogs</Link>
         </div>
       </header>
 
@@ -22,7 +22,7 @@ const page = () => {
             <p>The Creator of this website...</p>
           </div>
 
-          <div className="flex flex-col justify-center p-5 space-y-5 border lg:pt-5 lg:pb-5  lg:m-60 lg:mr-0 rounded-3xl h-auto">
+          <div className="flex ml-5 mr-5 flex-col justify-center p-5 space-y-5 border lg:pt-5 lg:pb-5  lg:m-60 lg:mr-0 rounded-3xl h-auto">
             <p className="text-xl font-bold underline underline-offset-4">About me:</p>
             <p className='text-sm'>As I Said Earlier I am Abhiram, recently graduated from Electrical Engineering...</p>
             <p className='text-xs flex justify-end'>View More...</p>
